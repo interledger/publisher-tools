9afc5dbacc1e5d598f6e665ccba6227fec8a1b1f
darian <darian410@gmail.com>
2025-07-14 14:48:46 +0300

    fix build

--- frontend/app/components/redesign/components/Checkbox.tsx
+++ frontend/app/components/redesign/components/Checkbox.tsx
@@ -10,7 +10,7 @@ export interface CheckboxProps {
   disabled?: boolean
 }
 
-export const Checkbox: React.FC<CheckboxProps> = ({
+export const CheckBox: React.FC<CheckboxProps> = ({
   checked = false,
   onChange,
   label,
@@ -59,4 +59,4 @@ export const Checkbox: React.FC<CheckboxProps> = ({
   )
 }
 
-export default Checkbox
+export default CheckBox
