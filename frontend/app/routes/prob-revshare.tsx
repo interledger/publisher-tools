a0cc88f21ca0cc80ec9eb3809c7f913761e6194d
Kenneth Mitchell <kenneth@interledger.org>
2025-07-14 22:03:34 -0400

    Minor styling fixes for information section and setup routing

--- frontend/app/routes/prob-revshare.tsx
+++ frontend/app/routes/prob-revshare.tsx
@@ -1,18 +1,20 @@
 import React, { useState } from 'react'
-
 import { HeadingCore } from '../components/redesign/components/HeadingCore'
 import { Heading5 } from '../components/redesign/Typography'
-
-import { SVGSpinner } from '~/assets/svg'
+import { SVGSpinner, SVGCopyScript } from '~/assets/svg'
 import { ToolsPrimaryButton } from '../components/redesign/components/ToolsPrimaryButton'
 import { ToolsSecondaryButton } from '../components/redesign/components/ToolsSecondaryButton'
 import { ShareInput, ShareInputMobile } from '../components/revshare/ShareInput'
 import { RevshareChart } from '../components/revshare/Chart'
 import { useShares, newShare, SharesProvider } from '../stores/revshareStore'
 
-import { sharesToPaymentPointer, changeList, dropIndex, weightFromPercent, trimDecimal } from '../lib/revshare'
-
-import { SVGCopyScript } from '~/assets/svg'
+import {
+  sharesToPaymentPointer,
+  changeList,
+  dropIndex,
+  weightFromPercent,
+  trimDecimal
+} from '../lib/revshare'
 
 export default function RevsharePageWrapper() {
   return (
@@ -49,7 +51,8 @@ function Revshare() {
   const { shares, setShares } = useShares()
   const totalWeight = shares.reduce((a, b) => a + Number(b.weight), 0)
 
-  const sharesPP = sharesToPaymentPointer(shares) || 'https://paymentpointer.example/alice'
+  const sharesPP =
+    sharesToPaymentPointer(shares) || 'https://paymentpointer.example/alice'
   const addShare = () => {
     setShares([...shares, newShare()])
   }
@@ -75,7 +78,11 @@ function Revshare() {
             onBackClick={() => console.log('Back clicked')}
             className="xl:mt-2xl"
           >
-            Probabilistic revenue sharing is a way to share a portion of a web monetized page&apos;s earnings between multiple wallet addresses and payment pointers. Each time a web monetized user visits the page, a recipient will be chosen at random. Payments will go to the chosen recipient until the page is closed or reloaded.
+            Probabilistic revenue sharing is a way to share a portion of a web
+            monetized page&apos;s earnings between multiple wallet addresses and
+            payment pointers. Each time a web monetized user visits the page, a
+            recipient will be chosen at random. Payments will go to the chosen
+            recipient until the page is closed or reloaded.
           </HeadingCore>
 
           <div className=" mx-auto space-y-lg">
@@ -86,11 +93,21 @@ function Revshare() {
               <table className="min-w-full  hidden md:table">
                 <thead className="bg-gray-200 font-normal">
                   <tr>
-                    <th className="text-left bg-gray-100 p-2 text-gray-400 font-normal rounded-tl-sm rounded-bl-sm">Name</th>
-                    <th className="text-left bg-gray-100 p-2 text-gray-400 font-normal">Payment Pointer ...</th>
-                    <th className="text-left bg-gray-100 p-2 text-gray-400 font-normal">Weight</th>
-                    <th className="text-left bg-gray-100 p-2 text-gray-400 font-normal">Percentage</th>
-                    <th className="text-left bg-gray-100 p-2 text-gray-400 font-normal rounded-tr-sm rounded-br-sm">Action</th>
+                    <th className="text-left bg-gray-100 p-2 text-gray-400 font-normal rounded-tl-sm rounded-bl-sm">
+                      Name
+                    </th>
+                    <th className="text-left bg-gray-100 p-2 text-gray-400 font-normal">
+                      Payment Pointer ...
+                    </th>
+                    <th className="text-left bg-gray-100 p-2 text-gray-400 font-normal">
+                      Weight
+                    </th>
+                    <th className="text-left bg-gray-100 p-2 text-gray-400 font-normal">
+                      Percentage
+                    </th>
+                    <th className="text-left bg-gray-100 p-2 text-gray-400 font-normal rounded-tr-sm rounded-br-sm">
+                      Action
+                    </th>
                   </tr>
                 </thead>
                 <tbody>
@@ -113,16 +130,22 @@ function Revshare() {
                         }
                         weightDisabled={!share.pointer}
                         percent={
-                          Number(share.weight) ? (share.weight || 1) / totalWeight : 100
+                          Number(share.weight)
+                            ? (share.weight || 1) / totalWeight
+                            : 100
                         }
                         percentDisabled={!share.pointer || shares.length <= 1}
                         onChangePercent={(percent) =>
                           setShares(
                             changeList(shares, i, {
                               weight: trimDecimal(
-                                weightFromPercent(percent, share.weight || 1, totalWeight),
-                              ),
-                            }),
+                                weightFromPercent(
+                                  percent,
+                                  share.weight || 1,
+                                  totalWeight
+                                )
+                              )
+                            })
                           )
                         }
                         onRemove={() => setShares(dropIndex(shares, i))}
@@ -134,7 +157,6 @@ function Revshare() {
               </table>
 
               <div className="md:hidden space-y-4">
-
                 {shares.map((share, i) => {
                   return (
                     <ShareInputMobile
@@ -154,16 +176,22 @@ function Revshare() {
                       }
                       weightDisabled={!share.pointer}
                       percent={
-                        Number(share.weight) ? (share.weight || 1) / totalWeight : 100
+                        Number(share.weight)
+                          ? (share.weight || 1) / totalWeight
+                          : 100
                       }
                       percentDisabled={!share.pointer || shares.length <= 1}
                       onChangePercent={(percent) =>
                         setShares(
                           changeList(shares, i, {
                             weight: trimDecimal(
-                              weightFromPercent(percent, share.weight || 1, totalWeight),
-                            ),
-                          }),
+                              weightFromPercent(
+                                percent,
+                                share.weight || 1,
+                                totalWeight
+                              )
+                            )
+                          })
                         )
                       }
                       onRemove={() => setShares(dropIndex(shares, i))}
@@ -188,7 +216,9 @@ function Revshare() {
                 <ToolsSecondaryButton
                   className="!border-none !p-0"
                   onClick={() => {
-                    navigator.clipboard.writeText(`<link rel="monetization" href="${sharesPP}"/>`)
+                    navigator.clipboard.writeText(
+                      `<link rel="monetization" href="${sharesPP}"/>`
+                    )
                     console.log('Payment Pointer copied to clipboard')
                   }}
                 >
@@ -198,7 +228,6 @@ function Revshare() {
             </div>
           </div>
           <div className="flex justify-end gap-4 mt-4">
-
             <ToolsSecondaryButton
               className="xl:w-[143px]"
               disabled={isLoading}
@@ -218,26 +247,56 @@ function Revshare() {
             >
               Add rev share
             </ToolsPrimaryButton>
-
           </div>
           <div className="w-full max-w-[1280px] px-md sm:px-lg md:px-xl lg:px-md">
             <div className="mt-4">
               <RevshareChart shares={shares} />
             </div>
           </div>
-          <div className=" text-gray-500 mt-4">
+          {/* Information section */}
+          <div className="flex flex-col justify-start gap-4 px-4">
             <Heading5>Information</Heading5>
-            <p className='text-base pt-2 leading-md text-text-primary'>
-              Each recipient has a different chance of being chosen, depending on their assigned weight. The weight is translated to a percentage which represents the percent of revenue each recipient will receive over time. The higher the weight, the larger the percentage.<br />
-              For example, if three recipients each have a weight of 1, then each recipient will eventually receive 33% of the revenue. If three recipients have a weight of 1, 2, and 3, the percentages will be 17% (weight 1), 33% (weight 2), and 50% (weight 3). <br />
-              Additional information can be found in the overview of the <a className="underline" href="https://webmonetization.org/tutorials/revenue-sharing" target="_blank" rel="noreferrer">Set up probabilistic revenue sharing</a> tutorial.
-            </p>
-            <br />
-            <p className='text-black font-bold'>Define a revshare</p>
-            <p className='text-base leading-md text-text-primary'>
-              Enter each payment pointer and wallet address that will receive a split of the revenue into the table. Names are optional. Click <a>Add Share</a> to add more rows. Assign a weight to each recipient. If you&apos;d rather assign sharing by percentage, enter at least two recipients into the table. The Percent field will open for edits.
-              When you&apos;re finished, add the generated monetization <a>link</a> tag to your site. The link contains a unique URL hosted on <a>https://webmonetization.org/api/revshare/pay/</a>.
-            </p>
+            <div>
+              <p className="text-sm leading-sm text-field-helpertext-default">
+                Each recipient has a different chance of being chosen, depending
+                on their assigned weight. The weight is translated to a
+                percentage which represents the percent of revenue each
+                recipient will receive over time. The higher the weight, the
+                larger the percentage.
+                <br />
+                For example, if three recipients each have a weight of 1, then
+                each recipient will eventually receive 33% of the revenue. If
+                three recipients have a weight of 1, 2, and 3, the percentages
+                will be 17% (weight 1), 33% (weight 2), and 50% (weight 3).{' '}
+                <br />
+                Additional information can be found in the overview of the{' '}
+                <a
+                  className="underline"
+                  href="https://webmonetization.org/tutorials/revenue-sharing"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Set up probabilistic revenue sharing
+                </a>{' '}
+                tutorial.
+              </p>
+            </div>
+            <div>
+              <p className="text-field-helpertext-default font-bold text-base leading-md">
+                Define a revshare
+              </p>
+              <p className="text-sm leading-sm text-field-helpertext-default">
+                Enter each payment pointer and wallet address that will receive
+                a split of the revenue into the table. Names are optional. Click{' '}
+                <a>Add Share</a> to add more rows. Assign a weight to each
+                recipient. If you&apos;d rather assign sharing by percentage,
+                enter at least two recipients into the table. The Percent field
+                will open for edits. When you&apos;re finished, add the
+                generated monetization <a>link</a> tag to your site. The link
+                contains a unique URL hosted on{' '}
+                <a>https://webmonetization.org/api/revshare/pay/</a>.
+              </p>
+            </div>
           </div>
         </div>
       </div>
