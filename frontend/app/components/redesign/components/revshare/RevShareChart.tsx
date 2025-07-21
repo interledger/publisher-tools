e665969cb5d84f3e4d0851e44827a26b3ed9c564
Kenneth Mitchell <kenneth@interledger.org>
2025-07-21 13:38:47 -0400

    Add code quality improvements

--- frontend/app/components/redesign/components/revshare/RevShareChart.tsx
+++ frontend/app/components/redesign/components/revshare/RevShareChart.tsx
@@ -31,8 +31,6 @@ export function RevShareChart({
       label={genLabel}
       radius={40}
       labelStyle={() => ({
-        fontFamily:
-          'system-ui, -apple-system, Segoe UI, Roboto, Ubuntu, Cantarell, Noto Sans, sans-serif, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
         fontSize: '6px'
       })}
       labelPosition={112}
