263b5495db2265727e91cb638d2d29c682e742d2
darian <darian410@gmail.com>
2025-08-05 13:59:32 +0300

    move isValidWA to shared utils

--- frontend/app/utils/validate.server.ts
+++ frontend/app/utils/validate.server.ts
@@ -2,7 +2,7 @@ import { z } from 'zod'
 import { CornerType, PositionType, SlideAnimationType } from '@shared/types'
 import {
   checkHrefFormat,
-  isWalletAddress,
+  isValidWalletAddress,
   toWalletAddressUrl,
   WalletAddressFormatError
 } from '@shared/utils'
@@ -137,34 +137,4 @@ export const validateForm = async (
   const payload = result.data as unknown as any
 
   return { result, payload }
-}
-
-async function isValidWalletAddress(
-  walletAddressUrl: string
-): Promise<boolean> {
-  const response = await fetch(walletAddressUrl, {
-    headers: {
-      Accept: 'application/json'
-    }
-  })
-
-  if (!response.ok) {
-    if (response.status === 404) {
-      throw new WalletAddressFormatError('This wallet address does not exist.')
-    }
-    throw new WalletAddressFormatError('Failed to fetch wallet address.')
-  }
-
-  const msgInvalidWalletAddress = 'Provided URL is not a valid wallet address.'
-  const json = await response.json().catch((error) => {
-    throw new WalletAddressFormatError(msgInvalidWalletAddress, {
-      cause: error
-    })
-  })
-
-  if (!isWalletAddress(json as Record<string, unknown>)) {
-    throw new WalletAddressFormatError(msgInvalidWalletAddress)
-  }
-
-  return true
 }
--- shared/utils/index.ts
+++ shared/utils/index.ts
@@ -100,3 +100,33 @@ export function checkHrefFormat(href: string): string {
 
   return href
 }
+
+export async function isValidWalletAddress(
+  walletAddressUrl: string
+): Promise<boolean> {
+  const response = await fetch(walletAddressUrl, {
+    headers: {
+      Accept: 'application/json'
+    }
+  })
+
+  if (!response.ok) {
+    if (response.status === 404) {
+      throw new WalletAddressFormatError('This wallet address does not exist.')
+    }
+    throw new WalletAddressFormatError('Failed to fetch wallet address.')
+  }
+
+  const msgInvalidWalletAddress = 'Provided URL is not a valid wallet address.'
+  const json = await response.json().catch((error) => {
+    throw new WalletAddressFormatError(msgInvalidWalletAddress, {
+      cause: error
+    })
+  })
+
+  if (!isWalletAddress(json as Record<string, unknown>)) {
+    throw new WalletAddressFormatError(msgInvalidWalletAddress)
+  }
+
+  return true
+}
