b6af1c9b00e3f806e641db1bf034996cfd166ce8
Kenneth Mitchell <kenneth@interledger.org>
2025-08-18 11:34:56 -0400

    Revert "Merge branch 'main' into 219-bug-rev-share-empty-table"
    This reverts commit 85cfbc7baa39bf6e1b6131028ba7dcdeebfae159, reversing
    changes made to 201f20b7c8b9de03f3dd925128d7ad17ce1bb7c1.

--- frontend/app/routes/prob-revshare.tsx
+++ frontend/app/routes/prob-revshare.tsx
@@ -126,6 +126,24 @@ function Revshare() {
     [shares, setShares]
   )
 
+  const handleChangePercent = useCallback(
+    (
+      index: number,
+      percent: number,
+      shareWeight: number,
+      totalWeight: number
+    ) => {
+      setShares(
+        changeList(shares, index, {
+          weight: trimDecimal(
+            weightFromPercent(percent, shareWeight || 1, totalWeight)
+          )
+        })
+      )
+    },
+    [shares, setShares]
+  )
+
   const handleRemove = useCallback(
     (index: number) => {
       const newShares = dropIndex(shares, index)
@@ -171,6 +189,15 @@ function Revshare() {
                     weightDisabled={!share.pointer}
                     percent={
                       totalWeight > 0 ? (share.weight || 0) / totalWeight : 0
+                    }
+                    percentDisabled={!share.pointer || shares.length <= 1}
+                    onChangePercent={(percent) =>
+                      handleChangePercent(
+                        i,
+                        percent,
+                        share.weight || 1,
+                        totalWeight
+                      )
                     }
                     onRemove={() => handleRemove(i)}
                     validatePointer={validatePointer}
