c13ad52c81cedaa483ea1e687b037950f474e968
Sid Vishnoi <8426945+sidvishnoi@users.noreply.github.com>
2025-08-22 14:31:26 +0530

    fix: update copyright to use W3C Community CLA

--- frontend/app/components/redesign/components/Footer.tsx
+++ frontend/app/components/redesign/components/Footer.tsx
@@ -1,21 +1,49 @@
-import { useLocation } from '@remix-run/react'
-import { EnhancedFooter } from '@/components'
-
 export const Footer = () => {
-  const location = useLocation()
-  const isIndexRoute = location.pathname === '/'
-
-  if (isIndexRoute) {
-    return <EnhancedFooter />
-  }
-
   return (
     <footer className="w-full pb-xl mt-2xl bg-interface-bg-main">
       <div className="flex flex-row items-center justify-center w-full">
-        <p className="!text-footer-content text-style-small-standard text-center">
-          Copyright© {new Date().getFullYear()} Interledger Foundation.
-        </p>
+        <Copyright />
       </div>
     </footer>
+  )
+}
+
+export function Copyright() {
+  return (
+    <p className="!text-field-helpertext-default text-style-small-standard text-center max-w-7xl px-4">
+      <CopyrightLink href="https://www.w3.org/policies/#copyright">
+        Copyright
+      </CopyrightLink>
+      © {new Date().getFullYear()} the Contributors to the Web Monetization
+      Specification, published by the{' '}
+      <CopyrightLink href="https://www.w3.org/community/wicg/">
+        Web Platform Incubator Community Group
+      </CopyrightLink>{' '}
+      under the{' '}
+      <CopyrightLink href="https://www.w3.org/community/about/agreements/cla/">
+        W3C Community Contributor License Agreement (CLA)
+      </CopyrightLink>
+      . A human-readable{' '}
+      <CopyrightLink href="http://www.w3.org/community/about/agreements/cla-deed/">
+        summary
+      </CopyrightLink>{' '}
+      is available.
+    </p>
+  )
+}
+
+function CopyrightLink({
+  href,
+  children
+}: React.PropsWithChildren<{ href: string }>) {
+  return (
+    <a
+      href={href}
+      target="_blank"
+      rel="noreferrer"
+      className="text-inherit underline"
+    >
+      {children}
+    </a>
   )
 }
