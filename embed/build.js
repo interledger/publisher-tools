271e9fbdf499ed19dcdc9f58a7f1a08ac11e4a11
Sid Vishnoi <8426945+sidvishnoi@users.noreply.github.com>
2025-09-16 16:42:46 +0530

    feat(cdn): add directory listing as 404 page (#322)

--- embed/build.js
+++ embed/build.js
@@ -1,5 +1,8 @@
+import path from 'node:path'
+import { writeFileSync } from 'node:fs'
 import { build } from 'esbuild'
 import { copy } from 'esbuild-plugin-copy'
+import directoryTree from 'directory-tree'
 
 const isDev = process.env.npm_lifecycle_script?.includes('--watch')
 
@@ -23,8 +26,74 @@ await build({
     '.svg': 'dataurl'
   },
   plugins: [
+    {
+      name: 'directory-tree',
+      setup(build) {
+        build.onEnd(() => {
+          const dir = build.initialOptions.outdir
+          const tree = directoryTree(dir, {
+            attributes: ['type'],
+            exclude: [/\b404\.html$/m, /\b_headers$/]
+          })
+
+          const content = generateDirectoryTreeHTML(tree)
+          const file = path.join(dir, '404.html')
+          writeFileSync(file, content, 'utf-8')
+        })
+      }
+    },
     copy({
       assets: [{ from: 'public/**', to: '.' }]
     })
   ]
 })
+
+/** @param {import('directory-tree').DirectoryTree} directoryTree */
+function generateDirectoryTreeHTML(directoryTree) {
+  const html = String.raw // for syntax highlighting
+
+  /** @param {string} p */
+  const pathToUrl = (p) => {
+    return p
+      .replace(/^dist./, '/')
+      .split(path.sep)
+      .map((s) => encodeURIComponent(s))
+      .join('/')
+  }
+
+  /** @param {import('directory-tree').DirectoryTree} node */
+  function buildTree(node) {
+    let htmlStr = `<ul>\n`
+    if (node.children) {
+      for (const child of node.children) {
+        if (child.type === 'file') {
+          const href = pathToUrl(child.path)
+          htmlStr += html`<li>
+            <a href="${href}">${child.name}</a>
+          </li>`
+        } else if (child.type === 'directory') {
+          htmlStr += html`<li>${child.name} ${buildTree(child)}</li>`
+        }
+      }
+    } else {
+      htmlStr += html`<li>${node.name}</li> `
+    }
+    htmlStr += `</ul>\n`
+    return htmlStr
+  }
+
+  return html`
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Directory Tree</title>
+      </head>
+      <body>
+        <h1>Directory Tree</h1>
+        ${buildTree(directoryTree)}
+      </body>
+    </html>
+  `
+}
