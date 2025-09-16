2ac8659a7ef1b5fad5087c00fdd07ada519f3b56
Sid Vishnoi <8426945+sidvishnoi@users.noreply.github.com>
2025-09-16 15:23:44 +0530

    feat(embed,cdn): add directory listing as 404 page

--- embed/build.js
+++ embed/build.js
@@ -1,8 +1,11 @@
-import { buildSync } from 'esbuild'
+import path from 'node:path'
+import { writeFileSync } from 'node:fs'
+import { build } from 'esbuild'
+import directoryTree from 'directory-tree'
 
 const isDev = process.env.npm_lifecycle_script?.includes('--watch')
 
-buildSync({
+await build({
   entryPoints: ['src/*.ts'],
   bundle: true,
   format: 'iife',
@@ -20,5 +23,76 @@ buildSync({
   loader: {
     '.css': 'text',
     '.svg': 'dataurl'
-  }
+  },
+  plugins: [
+    {
+      name: 'directory-tree',
+      setup(build) {
+        build.onEnd(() => {
+          const dir = build.initialOptions.outdir
+          const tree = directoryTree(dir, {
+            attributes: ['type'],
+            exclude: [/404\.html$/]
+          })
+
+          const content = generateDirectoryTreeHTML(tree)
+          const file = path.join(dir, '404.html')
+          writeFileSync(file, content, 'utf-8')
+        })
+      }
+    }
+  ]
 })
+
+/**
+ * @param {import('directory-tree').DirectoryTree} directoryTree
+ */
+function generateDirectoryTreeHTML(directoryTree) {
+  const html = String.raw // for syntax highlighting
+
+  /** @param {string} p */
+  const pathToUrl = (p) => {
+    return p
+      .replace(/^dist./, '/')
+      .split(path.sep)
+      .map((s) => encodeURIComponent(s))
+      .join('/')
+  }
+
+  function buildTree(node) {
+    let htmlStr = `<ul>\n`
+    if (node.children) {
+      for (const child of node.children) {
+        if (child.type === 'file') {
+          const href = pathToUrl(child.path)
+          htmlStr += html`<li>
+            <a href="${href}">${child.name}</a>
+          </li>`
+        } else if (child.type === 'directory') {
+          htmlStr += html`<li>${child.name} ${buildTree(child)}</li>`
+        }
+      }
+    } else {
+      htmlStr += html`<li>${node.name}</li> `
+    }
+    htmlStr += `</ul>\n`
+    return htmlStr
+  }
+
+  const htmlString = html`
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Directory Tree</title>
+      </head>
+      <body>
+        <h1>Directory Tree</h1>
+        ${buildTree(directoryTree)}
+      </body>
+    </html>
+  `
+
+  return htmlString
+}
