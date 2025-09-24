8f6e7b5658936d19d40b4571f0186f08113852f6
darian <darian410@gmail.com>
2025-09-24 14:56:13 +0300

    keep preset name

--- frontend/app/stores/toolStoreManager.ts
+++ frontend/app/stores/toolStoreManager.ts
@@ -1,27 +1,27 @@
-import { createDataStoreBanner, type BannerStoreType } from './bannerStore'
-import { createDataStoreWidget, type WidgetStoreType } from './widgetStore'
-import type { PresetIds } from '@shared/types'
+import { createDataStoreBanner } from './bannerStore'
+import { createDataStoreWidget } from './widgetStore'
+import type { BannerConfig, PresetIds, WidgetConfig } from '@shared/types'
 
 export class StoreManager {
-  private bannerStores: Record<PresetIds, BannerStoreType> = {
-    a: createDataStoreBanner('Banner preset 1'),
-    b: createDataStoreBanner('Banner preset 2'),
-    c: createDataStoreBanner('Banner preset 3')
+  private bannerStores: Record<PresetIds, BannerConfig> = {
+    a: createDataStoreBanner('Default preset 1'),
+    b: createDataStoreBanner('Default preset 2'),
+    c: createDataStoreBanner('Default preset 3')
   }
 
-  private widgetStores: Record<PresetIds, WidgetStoreType> = {
-    a: createDataStoreWidget('Widget preset 1'),
-    b: createDataStoreWidget('Widget preset 2'),
-    c: createDataStoreWidget('Widget preset 3')
+  private widgetStores: Record<PresetIds, WidgetConfig> = {
+    a: createDataStoreWidget('Default preset 1'),
+    b: createDataStoreWidget('Default preset 2'),
+    c: createDataStoreWidget('Default preset 3')
   }
 
   activeTab: PresetIds = 'a'
 
-  getBannerStore(key: PresetIds): BannerStoreType {
+  getBannerStore(key: PresetIds): BannerConfig {
     return this.bannerStores[key]
   }
 
-  getWidgetStore(key: PresetIds): WidgetStoreType {
+  getWidgetStore(key: PresetIds): WidgetConfig {
     return this.widgetStores[key]
   }
 }
