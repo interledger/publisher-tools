39fd08add4fb90408e4ae1605952ca2025ffff2f
darian <darian410@gmail.com>
2025-09-24 14:05:15 +0300

    clean

--- frontend/app/stores/bannerStore.ts
+++ frontend/app/stores/bannerStore.ts
@@ -1,10 +1,5 @@
 import { proxy } from 'valtio'
-// import type { BannerConfig } from '@shared/types'
 import { createDefaultBannerConfig } from '@shared/default-data'
-
-// export interface BannerStoreType {
-//   configuration: BannerConfig
-// }
 
 export const createDataStoreBanner = (presetName: string) =>
   proxy(createDefaultBannerConfig(presetName))
--- frontend/app/stores/widgetStore.ts
+++ frontend/app/stores/widgetStore.ts
@@ -1,10 +1,5 @@
 import { proxy } from 'valtio'
 import { createDefaultWidgetConfig } from '@shared/default-data'
-// import type { WidgetConfig } from '@shared/types'
-
-// export interface WidgetStoreType {
-//   configuration: WidgetConfig
-// }
 
 export const createDataStoreWidget = (presetName: string) =>
   proxy(createDefaultWidgetConfig(presetName))
