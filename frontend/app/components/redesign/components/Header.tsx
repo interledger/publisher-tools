2d45f040d26a051cad6d5106ce40784026c3f31a
adityagupta0251 <itsokkadi0251@gmail.com>
2025-10-02 23:17:28 +0530

    fix: update WM Logo link to redirect to Web Monetization homepage

--- frontend/app/components/redesign/components/Header.tsx
+++ frontend/app/components/redesign/components/Header.tsx
@@ -46,8 +46,8 @@ export const Header = () => {
           role="navigation"
           className="relative flex items-center justify-between bg-white shadow-[0px_12px_20px_0px_rgba(0,0,0,0.06)] rounded-lg mx-4 mt-4 h-14 py-1 pl-4 pr-2 md:max-w-7xl md:mx-auto md:px-xl md:py-xs md:mt-0 md:rounded-none md:shadow-none md:bg-transparent md:h-auto md:w-full"
         >
-          <RemixNavLink
-            to="/"
+          <a
+            href="https://webmonetization.org/"
             className="flex items-center focus:outline-none focus-visible:outline-2 focus-visible:outline-nav-link-hover focus-visible:outline-offset-0 rounded-sm"
           >
             <img
@@ -55,7 +55,7 @@ export const Header = () => {
               alt="Web Monetization Logo"
               className="w-8 md:w-11 h-8 md:h-11"
             />
-          </RemixNavLink>
+          </a>
 
           <ul className="hidden md:flex gap-md list-none">
             <NavDropdown title="Tools" />
