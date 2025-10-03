1f8f1ed2447629cc49467d98137d80a08b4e5910
darianm <47156919+DarianM@users.noreply.github.com>
2025-10-03 12:05:21 +0300

    update name

--- frontend/app/components/redesign/components/revshare/ShareInput.tsx
+++ frontend/app/components/redesign/components/revshare/ShareInput.tsx
@@ -84,8 +84,8 @@ export const ShareInputHeader = ({ showDelete }: { showDelete: boolean }) => {
         {showDelete && (
           <div
             role="columnheader"
-            id="col-delete"
-            aria-label="Delete recipient from table"
+            id="col-remove"
+            aria-label="Remove recipient from table"
           >
             Remove
           </div>
@@ -277,7 +277,7 @@ export const ShareInput = React.memo(
           >
             <ToolsSecondaryButton
               onClick={onRemove}
-              className="border-none py-sm px-xs shrink-0"
+              className="border-none p-xs shrink-0"
               aria-label="Remove recipient"
               aria-describedby={pointerInputId}
             >
