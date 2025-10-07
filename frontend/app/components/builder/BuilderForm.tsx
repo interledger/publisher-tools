0e69b3a48c6f63ce8f15ed06120ad97279b3a202
Sid Vishnoi <8426945+sidvishnoi@users.noreply.github.com>
2025-10-07 18:58:50 +0530

    refactor(frontend): remove indirection in banner builder & preview

---
+++ frontend/app/components/builder/BuilderForm.tsx
@@ -0,0 +1,63 @@
+import React, { useEffect } from 'react'
+import { BuilderPresetTabs } from '@/components'
+import { toolState, toolActions, type StableKey } from '~/stores/toolStore'
+import { useUI } from '~/stores/uiStore'
+import { useSnapshot } from 'valtio'
+
+interface Props {
+  onBuildStepComplete: (isComplete: boolean) => void
+}
+
+export function BuilderForm({
+  onBuildStepComplete,
+  children
+}: React.PropsWithChildren<Props>) {
+  const snap = useSnapshot(toolState)
+  const { state: uiState } = useUI()
+
+  useEffect(() => {
+    const bothComplete = uiState.contentComplete && uiState.appearanceComplete
+    if (onBuildStepComplete) {
+      onBuildStepComplete(bothComplete)
+    }
+  }, [uiState.contentComplete, uiState.appearanceComplete, onBuildStepComplete])
+
+  const handleTabSelect = (stableKey: StableKey) => {
+    toolActions.selectVersion(stableKey)
+  }
+
+  const getLatestTabOptions = () => {
+    return toolActions.versionOptions.map(({ stableKey: id }) => ({
+      id,
+      label: toolState.configurations[id].versionName,
+      isDirty: toolState.modifiedVersions.includes(id)
+    }))
+  }
+  const [tabOptions, setTabOptions] = React.useState(getLatestTabOptions)
+  const handleTabLabelChange = (stableKey: StableKey, newLabel: string) => {
+    toolActions.updateVersionLabel(stableKey, newLabel)
+    setTabOptions(() => getLatestTabOptions())
+  }
+
+  useEffect(() => {
+    setTabOptions(() => getLatestTabOptions())
+  }, [
+    toolState.isSubmitting,
+    toolState.currentConfig.walletAddress,
+    toolState.currentConfig.versionName
+  ])
+
+  return (
+    <div className="flex flex-col mt-4">
+      <BuilderPresetTabs
+        idPrefix="presets"
+        options={tabOptions}
+        selectedId={snap.activeVersion}
+        onChange={handleTabSelect}
+        onRename={handleTabLabelChange}
+      >
+        {children}
+      </BuilderPresetTabs>
+    </div>
+  )
+}
