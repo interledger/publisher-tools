6eba190f79f6dfe99233241b55ed77dd96e5a10b
Darian <darian410@gmail.com>
2025-10-21 11:17:39 +0300

    aria-label

--- frontend/app/components/redesign/components/Slider.tsx
+++ frontend/app/components/redesign/components/Slider.tsx
@@ -19,32 +19,26 @@ export const Slider: React.FC<Props> = ({
   id,
   className = '',
   step = 1
-}) => {
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    onChange(Number(e.target.value))
-  }
+}) => (
+  <div className={cx('relative w-full', className)}>
+    <div className="absolute inset-x-0 top-1/2 h-1 -translate-y-1/2 rounded-full bg-purple-100" />
+    <div
+      className="absolute top-1/2 size-6 -translate-y-1/2 rounded-full bg-white border-4 border-purple-300 pointer-events-none"
+      style={{
+        left: `calc(${((value - min) / (max - min)) * 100}% - 12px)`
+      }}
+    />
 
-  return (
-    <div className={cx('relative w-full', className)}>
-      <div className="absolute inset-x-0 top-1/2 h-1 -translate-y-1/2 rounded-full bg-purple-100" />
-      <div
-        className="absolute top-1/2 size-6 -translate-y-1/2 rounded-full bg-white border-4 border-purple-300 pointer-events-none"
-        style={{
-          left: `calc(${((value - min) / (max - min)) * 100}% - 12px)`
-        }}
-      />
-
-      <input
-        type="range"
-        id={id}
-        value={value}
-        min={min}
-        max={max}
-        step={step}
-        onChange={handleChange}
-        className="w-full opacity-0 cursor-pointer"
-        aria-label={`Slider from ${min} to ${max}`}
-      />
-    </div>
-  )
-}
+    <input
+      type="range"
+      id={id}
+      value={value}
+      min={min}
+      max={max}
+      step={step}
+      onChange={(e) => onChange(Number(e.target.value))}
+      className="w-full opacity-0 cursor-pointer"
+      aria-label={`Adjust font size from ${min} to ${max}, current value is ${value}`}
+    />
+  </div>
+)
