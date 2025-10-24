5d2eef0a92bdf040ba9650dbb35f1d8d4514b474
Kenneth Mitchell <kenneth@interledger.org>
2025-10-24 14:25:17 -0400

    fixes the crypto issue on local dev

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,38 +5,38 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 import { APP_BASEPATH } from './app/lib/constants.js'
 import path from 'path'
 
-export default defineConfig({
-  define: {
-    BUILD_CDN_URL: JSON.stringify(process.env.BUILD_CDN_URL),
-    BUILD_API_URL: JSON.stringify(process.env.BUILD_API_URL)
-  },
-  plugins: [
-    cloudflare({
-      persistState: {
-        path: '../.wrangler/v3'
-      }
-    }),
-    reactRouter(),
-    tsconfigPaths()
-  ],
-  resolve: {
-    alias: {
-      'crypto': 'crypto-browserify',
-      '@/components': path.resolve(
-        __dirname,
-        './app/components/redesign/components'
-      ),
-      '@/typography': path.resolve(
-        __dirname,
-        './app/components/redesign/Typography.tsx'
-      ),
-      '@/assets': path.resolve(__dirname, './app/assets/svg.tsx')
+export default defineConfig(({ mode, isSsrBuild }) => {
+  const isProduction = mode === 'production'
+  const alias: Record<string, string> = {
+    '@/components': path.resolve(
+      __dirname,
+      './app/components/redesign/components'
+    ),
+    '@/typography': path.resolve(
+      __dirname,
+      './app/components/redesign/Typography.tsx'
+    ),
+    '@/assets': path.resolve(__dirname, './app/assets/svg.tsx')
+  }
+
+  if (isProduction && !isSsrBuild) {
+    alias['crypto'] = 'crypto-browserify'
+  }
+
+  return {
+    define: {
+      BUILD_CDN_URL: JSON.stringify(process.env.BUILD_CDN_URL),
+      BUILD_API_URL: JSON.stringify(process.env.BUILD_API_URL)
+    },
+    plugins: [cloudflare(), reactRouter(), tsconfigPaths()],
+    resolve: {
+      alias
+    },
+    build: {
+      assetsDir: `${APP_BASEPATH.replace(/^\//, '')}/assets`,
+      sourcemap: true,
+      target: 'esnext',
+      outDir: 'build'
     }
-  },
-  build: {
-    assetsDir: `${APP_BASEPATH.replace(/^\//, '')}/assets`,
-    sourcemap: true,
-    target: 'esnext',
-    outDir: 'build'
   }
 })
