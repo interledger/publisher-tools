e98e395736a2bbe70ba04ed237118e7b8f366033
Sid Vishnoi <8426945+sidvishnoi@users.noreply.github.com>
2025-10-30 12:55:34 +0530

    Refactor Vite config to use concise syntax
    Avoids too many unnecessary whitespace changes in this PR

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,43 +5,39 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 import { APP_BASEPATH } from './app/lib/constants.js'
 import path from 'path'
 
-export default defineConfig(({ mode, isSsrBuild }) => {
-  const isProduction = mode === 'production'
-  const alias: Record<string, string> = {
-    '@/components': path.resolve(
-      __dirname,
-      './app/components/redesign/components'
-    ),
-    '@/typography': path.resolve(
-      __dirname,
-      './app/components/redesign/Typography.tsx'
-    ),
-    '@/assets': path.resolve(__dirname, './app/assets/svg.tsx'),
-    ...(isProduction && !isSsrBuild && { crypto: 'crypto-browserify' })
-  }
-
-  return {
-    define: {
-      BUILD_CDN_URL: JSON.stringify(process.env.BUILD_CDN_URL),
-      BUILD_API_URL: JSON.stringify(process.env.BUILD_API_URL)
-    },
-    plugins: [
-      cloudflare({
-        persistState: {
-          path: '../.wrangler/v3'
-        }
-      }),
-      reactRouter(),
-      tsconfigPaths()
-    ],
-    resolve: {
-      alias
-    },
-    build: {
-      assetsDir: `${APP_BASEPATH.replace(/^\//, '')}/assets`,
-      sourcemap: true,
-      target: 'esnext',
-      outDir: 'build'
+export default defineConfig(({ mode, isSsrBuild }) => ({
+  define: {
+    BUILD_CDN_URL: JSON.stringify(process.env.BUILD_CDN_URL),
+    BUILD_API_URL: JSON.stringify(process.env.BUILD_API_URL)
+  },
+  plugins: [
+    cloudflare({
+      persistState: {
+        path: '../.wrangler/v3'
+      }
+    }),
+    reactRouter(),
+    tsconfigPaths()
+  ],
+  resolve: {
+    alias: {
+      '@/components': path.resolve(
+        __dirname,
+        './app/components/redesign/components'
+      ),
+      '@/typography': path.resolve(
+        __dirname,
+        './app/components/redesign/Typography.tsx'
+      ),
+      '@/assets': path.resolve(__dirname, './app/assets/svg.tsx'),
+      ...(mode === 'production' &&
+        !isSsrBuild && { crypto: 'crypto-browserify' })
     }
+  },
+  build: {
+    assetsDir: `${APP_BASEPATH.replace(/^\//, '')}/assets`,
+    sourcemap: true,
+    target: 'esnext',
+    outDir: 'build'
   }
-})
+}))
