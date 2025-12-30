f400a5b248b13338bd0684a7d623fe937c0d7fb5
Darian <darian410@gmail.com>
2025-12-30 13:44:08 +0200

    better file indent

--- frontend/app/components/redesign/components/SaveResultModal.tsx
+++ frontend/app/components/redesign/components/SaveResultModal.tsx
@@ -15,43 +15,45 @@ export const StatusModal: React.FC<SaveResultModalProps> = ({
   message = 'Your edits have been saved',
   fieldErrors,
   status
-}) => (
-  <BaseModal
-    className="p-8 pb-4
+}) => {
+  return (
+    <BaseModal
+      className="p-8 pb-4
       flex flex-col items-center gap-6 w-[426px]"
-  >
-    <div className="flex items-center justify-center">
-      {status === 'error' || fieldErrors ? (
-        <SVGErrorVector className="w-[60px] h-[60px]" />
-      ) : (
-        <SVGMarkSuccess className="w-[60px] h-[60px]" />
+    >
+      <div className="flex items-center justify-center">
+        {status === 'error' || fieldErrors ? (
+          <SVGErrorVector className="w-[60px] h-[60px]" />
+        ) : (
+          <SVGMarkSuccess className="w-[60px] h-[60px]" />
+        )}
+      </div>
+      <div className="text-center">
+        <p className="text-base leading-md font-normal text-text-primary">
+          {message}
+        </p>
+      </div>
+      {fieldErrors && Object.keys(fieldErrors).length && (
+        <details className="self-start text-left">
+          <ul className="list-disc ml-6 text-sm text-field-helpertext-default">
+            {Object.entries(fieldErrors).map(([key, msg], i) => (
+              <li key={i}>
+                <span className="font-medium">{key}</span>: {msg as string}
+              </li>
+            ))}
+          </ul>
+        </details>
       )}
-    </div>
-    <div className="text-center">
-      <p className="text-base leading-md font-normal text-text-primary">
-        {message}
-      </p>
-    </div>
-    {fieldErrors && Object.keys(fieldErrors).length && (
-      <details className="self-start text-left">
-        <ul className="list-disc ml-6 text-sm text-field-helpertext-default">
-          {Object.entries(fieldErrors).map(([key, msg], i) => (
-            <li key={i}>
-              <span className="font-medium">{key}</span>: {msg as string}
-            </li>
-          ))}
-        </ul>
-      </details>
-    )}
-    <div className="w-full">
-      <ToolsSecondaryButton
-        className="w-full flex items-center justify-center"
-        onClick={onDone}
-      >
-        Done
-      </ToolsSecondaryButton>
-    </div>
-  </BaseModal>
-)
+      <div className="w-full">
+        <ToolsSecondaryButton
+          className="w-full flex items-center justify-center"
+          onClick={onDone}
+        >
+          Done
+        </ToolsSecondaryButton>
+      </div>
+    </BaseModal>
+  )
+}
 
 export default StatusModal
