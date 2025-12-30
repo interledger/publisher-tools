d68e6b205ad2fc9fb6b17ae8c9d597bd1e1765d6
Darian <darian410@gmail.com>
2025-12-30 14:01:47 +0200

    remove overkill reducer since no complex logic

--- frontend/app/components/redesign/components/modals/DialogProvider.tsx
+++ frontend/app/components/redesign/components/modals/DialogProvider.tsx
@@ -1,68 +1,26 @@
-import { createContext, useReducer, type ReactNode } from 'react'
-
-type DialogState = {
-  isOpen: boolean
-  dialog: ReactNode | null
-}
-
-type DialogAction =
-  | {
-      type: 'OPEN'
-      data: {
-        isOpen: true
-        dialog: ReactNode
-      }
-    }
-  | {
-      type: 'CLOSE'
-      data: {
-        isOpen: false
-        dialog: null
-      }
-    }
-
-const dialogInitialState: DialogState = {
-  isOpen: false,
-  dialog: null
-}
+import { createContext, useState, type ReactNode } from 'react'
 
 export const DialogContext = createContext<{
-  state: DialogState
-  dispatch: React.Dispatch<DialogAction>
+  openDialog: (dialog: ReactNode) => void
+  closeDialog: () => void
 }>({
-  state: dialogInitialState,
-  dispatch: () => null
+  openDialog: () => null,
+  closeDialog: () => null
 })
 
-const reducer = (state: DialogState, action: DialogAction): DialogState => {
-  switch (action.type) {
-    case 'OPEN':
-      return {
-        ...state,
-        isOpen: action.data.isOpen,
-        dialog: action.data.dialog
-      }
-    case 'CLOSE':
-      return {
-        ...state,
-        isOpen: action.data.isOpen,
-        dialog: action.data.dialog
-      }
-  }
-}
-
 export const DialogProvider = ({ children }: { children: ReactNode }) => {
-  const [state, dispatch] = useReducer(reducer, dialogInitialState)
+  const [dialog, setDialog] = useState<ReactNode | null>(null)
+
+  const openDialog = (content: ReactNode) => setDialog(content)
+  const closeDialog = () => setDialog(null)
 
   const Dialog = () => {
-    if (!state.isOpen) return null
-    if (!state.dialog) return null
-
-    return state.dialog
+    if (!dialog) return null
+    return dialog
   }
 
   return (
-    <DialogContext.Provider value={{ state, dispatch }}>
+    <DialogContext.Provider value={{ openDialog, closeDialog }}>
       <Dialog />
       {children}
     </DialogContext.Provider>
