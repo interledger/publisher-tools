d2ae7e3f65b0ca6a4e3b8fd8e4e27803ee413d7c
Darian <darian410@gmail.com>
2026-02-03 14:55:33 +0200

    li Step; export default; id

--- frontend/app/components/offerwall/HowItWorks.tsx
+++ frontend/app/components/offerwall/HowItWorks.tsx
@@ -16,7 +16,7 @@ const Step: React.FC<StepProps> = ({
   description,
   showNext = false,
 }) => (
-  <>
+  <li className="flex flex-col items-center xl:flex-1 xl:flex-row xl:items-center">
     <div className="flex w-[343px] flex-col items-center gap-2xl xl:w-[296px] xl:max-h-[240px] xl:flex-1">
       <img src={image} alt="" className="h-full w-auto object-contain" />
       <BodyEmphasis className="min-w-full shrink-0 text-center !text-green-400">
@@ -29,17 +29,22 @@ const Step: React.FC<StepProps> = ({
         <SVGStepArrowMobile className="h-[63px] w-[15px] shrink-0 self-center block xl:hidden" />
       </>
     )}
-  </>
+  </li>
 )
 
-const HowItWorks: React.FC = () => {
+export default function HowItWorks() {
   return (
     <section
       className="flex max-w-[1280px] flex-col gap-2xl rounded-md px-md"
       aria-labelledby="how-it-works-heading"
     >
       <header className="flex flex-col items-center gap-md xl:items-start">
-        <Heading4 className="xl:text-style-h2-semibold">How it works</Heading4>
+        <Heading4
+          id="how-it-works-heading"
+          className="xl:text-style-h2-semibold"
+        >
+          How it works
+        </Heading4>
         <BodyStandard className="text-center xl:text-left xl:text-xl xl:leading-xl">
           If you have a Google Ad Manager account you can add Web Monetization
           to your Offerwall.
@@ -53,31 +58,21 @@ const HowItWorks: React.FC = () => {
         className="flex w-full list-none flex-col items-center p-0 xl:flex-row xl:items-center"
         aria-label="Steps to use Web Monetization with Google Offerwall"
       >
-        <li className="flex flex-col items-center xl:flex-1 xl:flex-row xl:items-center">
-          <Step
-            image={step1}
-            description="Customize your Offerwall experience"
-            showNext
-          />
-        </li>
-
-        <li className="flex flex-col items-center xl:flex-1 xl:flex-row xl:items-center">
-          <Step
-            image={step2}
-            description="Copy your customization into your Offerwall"
-            showNext
-          />
-        </li>
-
-        <li className="flex flex-col items-center xl:flex-1 xl:flex-row xl:items-center">
-          <Step
-            image={step3}
-            description="Start receiving support as users choose to use WM"
-          />
-        </li>
+        <Step
+          image={step1}
+          description="Customize your Offerwall experience"
+          showNext
+        />
+        <Step
+          image={step2}
+          description="Copy your customization into your Offerwall"
+          showNext
+        />
+        <Step
+          image={step3}
+          description="Start receiving support as users choose to use WM"
+        />
       </ol>
     </section>
   )
 }
-
-export default HowItWorks
