c344e56553eee098b66054152499137a2f77b36d
Darian <darian410@gmail.com>
2026-02-09 17:08:17 +0200

    use cx utility divider

--- frontend/app/components/redesign/components/Divider.tsx
+++ frontend/app/components/redesign/components/Divider.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import { cx } from 'class-variance-authority'
 
 export const Divider: React.FC<{ className?: string }> = ({ className }) => {
-  return <div className={`w-full h-px bg-silver-200 my-md ${className}`}></div>
+  return (
+    <div className={cx('w-full h-px bg-silver-200 my-md', className)}></div>
+  )
 }
 
 export default Divider
