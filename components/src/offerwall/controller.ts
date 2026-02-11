5fef3a9c3224198e551d3248d374fc04727e3751
Sid Vishnoi <8426945+sidvishnoi@users.noreply.github.com>
2026-02-11 15:27:40 +0530

    feat(offerwall): support preview mode for consumer components

--- components/src/offerwall/controller.ts
+++ components/src/offerwall/controller.ts
@@ -4,6 +4,11 @@ export interface Controller {
   onModalClose(ev: Event): void
   onExtensionLinkClick(ev: Event): void
   onDone(ev: Event): void
+  /**
+   * In preview mode, instead of rendering the component as "dialog" modal;  we
+   * render it in a way so it can be embedded in editor's preview interface.
+   */
+  isPreviewMode?: boolean
 
   // TODO: add all handlers to controller only, instead of using the element
   // instance. The element instance should have `setController` as the only
