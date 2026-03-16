dba4e8ffd85995756b732748a88b62c47c174373
Sid Vishnoi <8426945+sidvishnoi@users.noreply.github.com>
2026-03-16 19:11:35 +0530

    fix(/offerwall): add back view docs link (#616)

--- frontend/app/components/redesign/components/dialogs/ScriptDialog.tsx
+++ frontend/app/components/redesign/components/dialogs/ScriptDialog.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { useSnapshot } from 'valtio'
 import { SVGMarkStatusSmall, SVGTooltip } from '@/assets'
 import { ToolsPrimaryButton } from '@/components'
-import { TOOL_OFFERWALL } from '@shared/types'
 import { toWalletAddressUrl } from '@shared/utils'
 import { toolState } from '~/stores/toolStore'
 import { BaseDialog } from './BaseDialog'
@@ -76,10 +75,7 @@ export const ScriptDialog: React.FC = () => {
         </ToolsPrimaryButton>
       </div>
 
-      <p
-        hidden={snap.currentToolType === TOOL_OFFERWALL}
-        className="text-style-small-standard text-text-primary text-center"
-      >
+      <p className="text-style-small-standard text-text-primary text-center">
         View{' '}
         <a
           href={`https://webmonetization.org/publishers/${snap.currentToolType}-tool/`}
