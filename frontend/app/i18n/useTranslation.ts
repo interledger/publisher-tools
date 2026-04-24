cadbabdf523e1bbdbf5a0d473f956593b0697bc3
Darian <darian410@gmail.com>
2026-04-24 15:25:21 +0300

    add safeguard

--- frontend/app/i18n/useTranslation.ts
+++ frontend/app/i18n/useTranslation.ts
@@ -6,7 +6,9 @@ type NamespaceKey<NS extends Namespace> = keyof Translations[NS] & string
 type Params = Record<string, string>
 
 function interpolate(str: string, params: Params): string {
-  return str.replace(/\{\{(\w+)\}\}/g, (_, key) => params[key] ?? `{{${key}}}`)
+  return str.replace(/\{\{(\w+)\}\}/g, (_, key) =>
+    Object.hasOwn(params, key) ? String(params[key]) : `{{${key}}}`,
+  )
 }
 
 export function useTranslation<NS extends Namespace>(ns: NS) {
