b17f0bce9e6d972f107c6eeabf18c052b989c8db
Sid Vishnoi <8426945+sidvishnoi@users.noreply.github.com>
2026-05-20 20:11:40 +0530

    feat(paywall): store payments in db for future entitlement

---
+++ api/src/routes/paywall/redirect.ts
@@ -0,0 +1,56 @@
+import z from 'zod'
+import { urlWithParams } from '@shared/utils'
+import { app } from '../../app'
+import { PaymentIdSchema } from '../../schemas/payment'
+import { savePayment } from '../../utils/payments-db'
+import { getData } from '../../utils/payments-kv'
+import { validate } from '../../utils/utils'
+
+const schema = z.object({
+  // Passed by client
+  next: z.url(),
+  // Set by /payment/redirect
+  paymentId: PaymentIdSchema,
+  result: z.enum(['success', 'failure']),
+})
+
+app.get('/paywall/redirect', validate('query', schema), async (c) => {
+  const { req, redirect, env } = c
+  const { next, paymentId, result } = req.valid('query')
+
+  if (result === 'failure') {
+    return redirect(urlWithParams(next, { paymentId, result }))
+  }
+
+  const data = await getData(c.env.PUBLISHER_TOOLS_KV, paymentId)
+  if (!data) {
+    console.error(`Missing payment info: ${paymentId}`)
+    return redirect(
+      urlWithParams(next, { paymentId, result: 'failure', reason: 'internal' }),
+    )
+  }
+  if (data.status === 'PENDING' || data.status === 'GRANT_REJECTED') {
+    throw new Error(`Unexpected payment status: ${data.status}`)
+  }
+  const { incomingPaymentId, outgoingPaymentId, receiver, sender } = data
+
+  try {
+    await savePayment(env.PUBLISHER_TOOLS_DB, {
+      paymentId,
+      url: new URL(next),
+      incomingPaymentId,
+      outgoingPaymentId,
+      sender,
+      receiver,
+      status: data.status,
+      amount: data.amount,
+    })
+  } catch (err) {
+    console.error(err)
+    return redirect(
+      urlWithParams(next, { paymentId, status: 'failure', reason: 'internal' }),
+    )
+  }
+
+  return redirect(urlWithParams(next, { paymentId, result }))
+})
