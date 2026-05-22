ce4965330cad969b9f18f8b9e8cd745c245be4b5
Sid Vishnoi <8426945+sidvishnoi@users.noreply.github.com>
2026-05-22 14:46:36 +0530

    utils: fix getPageUrl regex pattern

--- cdn/src/utils/paywall-utils.ts
+++ cdn/src/utils/paywall-utils.ts
@@ -2,6 +2,6 @@ import { ensureEnd } from '@shared/utils'
 
 export function getPageUrl(url: URL | Location) {
   const { origin, pathname } = url
-  const path = /\.html?^/.test(pathname) ? pathname : ensureEnd(pathname, '/')
+  const path = /\.html?$/.test(pathname) ? pathname : ensureEnd(pathname, '/')
   return origin + path
 }
