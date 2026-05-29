c8e670eac07fbd03f0ae15111c77fc2faf46b360
Kenneth Mitchell <kenneth@interledger.org>
2026-05-28 23:51:53 -0400

    Add inspector ports to wrangler files to avoid builds not running

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -27,6 +27,7 @@ export default defineConfig(({ mode, isSsrBuild }) => ({
       persistState: {
         path: '../.wrangler',
       },
+      inspectorPort: 9232,
     }),
     reactRouter(),
   ],
