3ac48dd113dc58ed6ba37c25742c9a9724a121cc
Sid Vishnoi <8426945+sidvishnoi@users.noreply.github.com>
2026-06-02 19:47:41 +0530

    rename postMessage to `updateUI`

--- frontend/app/components/paywall/PaywallPreview.tsx
+++ frontend/app/components/paywall/PaywallPreview.tsx
@@ -16,17 +16,17 @@ export function PaywallPreview() {
   useEffect(() => {
     window.addEventListener(
       'message',
-      (ev) => ev.origin === location.origin && postMessage(ref.current),
+      (ev) => ev.origin === location.origin && updateUI(ref.current),
       { once: true },
     )
-    return subscribe(paywall.profile, () => postMessage(ref.current))
+    return subscribe(paywall.profile, () => updateUI(ref.current))
   }, [])
 
   return (
     <BuilderBackground iframeMode={true}>
       <iframe
         ref={ref}
-        onLoad={(ev) => postMessage(ev.currentTarget)}
+        onLoad={(ev) => updateUI(ev.currentTarget)}
         src="/tools/paywall/preview"
         className="w-full overflow-hidden border-none"
         style={{ height: 'clamp(25rem, 85vh, 46rem)' }}
@@ -35,10 +35,13 @@ export function PaywallPreview() {
   )
 }
 
-function postMessage(iframeEl: HTMLIFrameElement | null) {
-  const message: Message = {
-    action: 'UPDATE',
-    profile: deepClone(paywall.profile),
-  }
-  iframeEl?.contentWindow!.postMessage(message, location.origin)
+function updateUI(iframeEl: HTMLIFrameElement | null) {
+  if (!iframeEl) return
+  const profile = deepClone(paywall.profile)
+  const message: Message = { action: 'UPDATE', profile }
+  postMessage(iframeEl, message)
+}
+
+function postMessage(iframeEl: HTMLIFrameElement, message: Message) {
+  iframeEl.contentWindow!.postMessage(message, location.origin)
 }
