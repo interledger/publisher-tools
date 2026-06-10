a32cc0a75c69fa9516b87ce6381ddd1a29cb929c
Sid Vishnoi <8426945+sidvishnoi@users.noreply.github.com>
2026-06-10 17:13:24 +0530

    fixup! editor form improvements

--- frontend/app/components/paywall/PaywallBuilderSettings.tsx
+++ frontend/app/components/paywall/PaywallBuilderSettings.tsx
@@ -5,8 +5,8 @@ import { Heading5 } from '../redesign/Typography'
 
 export function PaywallBuilderSettings() {
   return (
-    <div className="bg-white shadow-sm rounded-xl p-4 space-y-sm">
-      <Heading5>Settings</Heading5>
+    <div className="bg-white shadow-sm rounded-xl p-4">
+      <Heading5 className="mb-sm">Settings</Heading5>
 
       <PaywallPriceBuilder />
       <Divider className="mt-md mb-lg" />
